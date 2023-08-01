4f9c676f5ce968a508ff720a8a08f3ce7db07280
Hiep <tcmytt@gmail.com>
2023-08-01 09:42:18 +0700

    lastest

--- react-webpack/src/index.js
+++ react-webpack/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react' // nạp thư viện react
-import ReactDOM from 'react-dom' // nạp thư viện react-dom
+import ReactDOM from 'react-dom/client' // nạp thư viện react-dom
 
 // Tạo component App
 function App() {
@@ -11,5 +11,11 @@ function App() {
 }
 
 // Render component App vào #root element
-ReactDOM.render(<App />, document.getElementById('root'))
+
+// react-dom@17
+//ReactDOM.render(<App />, document.getElementById('root'))
+
+// react-dom@18
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
 
