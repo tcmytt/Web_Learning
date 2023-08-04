43845aa737aca94f5d7df56dae388d3035e28872
Hiep <tcmytt@gmail.com>
2023-08-04 16:13:40 +0700

    4/8 2

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -6,9 +6,11 @@ function Content() {
   const [title, setTitle] = useState("");
   const [posts, setPosts] = useState([]);
   const [type, setType] = useState("posts");
+  const [gotop, setGotop] = useState(false);
+
+  console.log(type);
 
   useEffect(() => {
-    console.log(type);
     fetch(`https://jsonplaceholder.typicode.com/${type}`)
       .then((res) => res.json())
       .then((posts) => {
@@ -16,31 +18,61 @@ function Content() {
       });
   }, [type]);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 500) {
+        setGotop(true);
+      } else {
+        setGotop(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div>
-      {tabs.map((tab, index) => (
+      {tabs.map((tab) => (
         <button
-          key={index}
-          style={
-            type === tab
-              ? {
-                  color: "#fff",
-                  backgroundColor: "#333"
-                }
-              : {}
-          }
+          key={tab}
+          style={type === tab ? { color: "#fff", background: "#333" } : {}}
           onClick={() => setType(tab)}
         >
           {tab}
         </button>
       ))}
 
-      <input value={title} onChange={(e) => setTitle(e.target.value)}></input>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title || post.name}</li>
-        ))}
-      </ul>
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+
+      {posts.map((post) => (
+        <li key={post.id}>{post.title || post.name}</li>
+      ))}
+
+      {gotop && (
+        <button
+          style={{
+            right: 0,
+            bottom: 0,
+            position: "fixed",
+            borderRadius: "99px",
+            width: "100px",
+            height: "50px",
+            background: "#333",
+            color: "#fff",
+            textAlign: "center",
+            cursor: "pointer",
+            border: "none"
+          }}
+          onClick={() =>
+            window.scrollTo({
+              top: 0,
+              behavior: "smooth"
+            })
+          }
+        >
+          Gotop
+        </button>
+      )}
     </div>
   );
 }
