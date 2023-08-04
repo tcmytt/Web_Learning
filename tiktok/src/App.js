2d1f791b63e30be3d801ef751b863b46323052fd
Hiep <tcmytt@gmail.com>
2023-08-04 10:32:30 +0700

    4/8 1

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,38 +1,45 @@
 import {useState} from 'react'
 
 function App() {
-  const [state, setState] = useState(1);
 
-
-  let handleIncrease =() => {
-    // Không áp dụng liên tục dù gọi 3 lần
-    setState(state + 1)
-    setState(state + 1)
-    setState(state + 1)
-
-    // Áp dụng liên tục gọi cả 3 lần
-    setState(prevState => prevState +1 )
-    setState(prevState => prevState +1 )
-    setState(prevState => prevState +1 )
-
-  }
-
-  let handleUpdate = () =>{
-    setState(pre =>{
-      // logic
-
-      return {
-        //...
-      }
-    })
-  }
-
-  return (
-    <div style = {{padding : 20 }} className="App">
-      <h1  >{state} </h1>
-      <button onClick={handleIncrease}>Increase</button>
-    </div>
-  );
+  const courses = [
+    { id: 1, name: "HTML,CSS", price: 100 },
+    { id: 2, name: "JavaScript", price: 120 },
+    { id: 3, name: "ReactJS", price: 150 }
+  ];   
+  
+    const [checked, setChecked] = useState([]);
+    console.log({ ids: checked });
+    const handleCheck = (id) => {
+      const isChecked = checked.includes(id);
+      setChecked((prev) => {
+        if (isChecked) {
+          return checked.filter((item) => item !== id);
+        } else {
+          return [...prev, id];
+        }
+      });
+    };
+    const handleSubmit = () => {
+      //Call API
+    };
+    return (
+      <div className="App">
+        {courses.map((course) => (
+          <div key={course.id}>
+            <input
+              type="checkbox"
+              checked={checked.includes(course.id)}
+              onChange={() => handleCheck(course.id)}
+            />
+            {course.name}
+          </div>
+        ))}
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+    );
+  
+  
 }
 
 export default App;
