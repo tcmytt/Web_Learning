5bbf65a1873e17d3e3fc0d4e23324d2a5dd18e5e
Hiep <tcmytt@gmail.com>
2023-08-27 19:40:49 +0700

    27 8

--- Node_ExpressJS/src/index.js
+++ Node_ExpressJS/src/index.js
@@ -4,40 +4,41 @@ const path = require('path');
 
 const { engine } = require('express-handlebars');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // import route from './routes';
-const route = require('./routes')
+const route = require('./routes');
 
-app.use(express.urlencoded({
-  extended : true
-}))
-app.use(express.json())
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
 // HTTP logger
 //app.use(morgan('combined'))
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 // Set lại đường dẫn file views
-app.set('views',path.join(__dirname,'resources\\views'))
+app.set('views', path.join(__dirname, 'resources\\views'));
 
 // Xử lí static file Set đường dẫn ảnh
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set lại đuôi file cho đỡ dài và chạy lại engine
-app.engine('.hbs', engine({extname: '.hbs'}));
+app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
 // route init
 route(app);
 
-
 app.post('/search', (req, res) => {
-  //  console.log(req.query)
+    //  console.log(req.query)
     res.render('search');
-  })
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
