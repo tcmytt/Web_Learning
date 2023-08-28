b264108deb74b943b071072bc8bf3ae0c53ef34e
Hiep <tcmytt@gmail.com>
2023-08-28 16:24:32 +0700

    28 8

--- Node_ExpressJS/src/app/controllers/SiteController.js
+++ Node_ExpressJS/src/app/controllers/SiteController.js
@@ -1,16 +1,20 @@
+const Course = require('../models/Course');
 
 class SiteController {
-
     // [GET] /
-    index (req,res) {
-        res.render('home');
+    async index(req, res) {
+        try {
+            res.json(await Course.find({}));
+        } catch (error) {
+            res.status(400).json({ error: 'Fail to get Course' });
+        }
+        // res.render("home")
     }
 
     // [GET] /search
-    search (req,res){
+    search(req, res) {
         res.render('search');
     }
 }
 
-module.exports = new SiteController
-
+module.exports = new SiteController();
