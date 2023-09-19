de7d9d03fd7f5a2bf2d699fc22421f0db510f6ba
Hiep <tcmytt@gmail.com>
2023-09-19 22:49:37 +0700

    19 8

--- tiktok-ui/src/components/Layout/components/Header/index.js
+++ tiktok-ui/src/components/Layout/components/Header/index.js
@@ -1,5 +1,11 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import images from '~/assets/images';
+
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
 
 const cx = classNames.bind(styles);
 
@@ -7,8 +13,21 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                {/* logo */}
-                {/* Search */}
+                <img src={images.logo} alt="Tiktok" />
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <tippy content="Tìm kiếm">
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </tippy>
+                </div>
+                <div className={cx('actions')}></div>
             </div>
         </header>
     );
