bbdc7779d4a83d4b6fb8b3e282992629760ee37b
NguyenHiep <gajxinh176@gmail.com>
2024-03-27 08:00:13 +0700

    25/3

---
+++ TypeScript/hello.ts
@@ -0,0 +1,2 @@
+var fullName: string;                   //Khai báo biến với TS cho phép chỉ định kiểu cụ thể
+console.log('Xin chào, ' + fullName);
---
+++ TypeScript/module1.ts
@@ -0,0 +1,9 @@
+var var1:string = 'A';
+function fun1() {
+    console.log('f1');
+}
+class classExample {
+    property1: string
+}
+
+export {var1, fun1, classExample};
